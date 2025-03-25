ecbe897c14c8ee0ac5d05c9764d2f2eb77f1c4f2
Talkar <2336860+Talkar@users.noreply.github.com>
2025-03-25 19:21:26 +0100

    Setup nvim-dap events to trigger nvim-dap ui

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,23 +1,38 @@
 
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-    "rcarriga/nvim-dap-ui",  -- Interface for nvim-dap
-    {
-      "theHamsta/nvim-dap-virtual-text",  -- Adds virtual text annotations for DAP
-      opts = {},
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "rcarriga/nvim-dap-ui",  -- Interface for nvim-dap
+      {
+        "theHamsta/nvim-dap-virtual-text",  -- Adds virtual text annotations for DAP
+        opts = {},
+      },
     },
-  },
-  config = function()
-    -- Set up nvim-dap-ui (visual interface for nvim-dap)
-    local dap_ui = require("dapui")
-    dap_ui.setup()
+    config = function()
+      -- Set up nvim-dap-ui (visual interface for nvim-dap)
+      local dap_ui = require("dapui")
+      dap_ui.setup()
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
 
-    -- Set up nvim-dap-virtual-text (displays virtual text for debugging)
-    local dap_virtual_text = require("nvim-dap-virtual-text")
-    dap_virtual_text.setup()
+      -- Set up nvim-dap-virtual-text (displays virtual text for debugging)
+      local dap_virtual_text = require("nvim-dap-virtual-text")
+      dap_virtual_text.setup()
 
-    -- Optionally, you can configure other things like custom keybindings or UI settings here
-  end,
+      -- Optionally, you can configure other things like custom keybindings or UI settings here
+    end,
+  }
 }
