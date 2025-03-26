55eb10cf2b7f87610e8f6b655c45fc089574bc8d
Talkar <2336860+Talkar@users.noreply.github.com>
2025-03-26 20:04:39 +0100

    Added icons to dap breakpoints

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -14,19 +14,24 @@ return {
       -- Set up nvim-dap-ui (visual interface for nvim-dap)
       local dap_ui = require("dapui")
       dap_ui.setup()
-local dap, dapui = require("dap"), require("dapui")
-dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
+      local dap, dapui = require("dap"), require("dapui")
+
+      vim.fn.sign_define('DapBreakpoint',{ text ='🔴', texthl ='', linehl ='', numhl =''})
+      vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
+
+      -- Open and close dap-ui automatically when debugging
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
       -- Set up nvim-dap-virtual-text (displays virtual text for debugging)
       local dap_virtual_text = require("nvim-dap-virtual-text")
